3ad1145d27010043a960d317e12180237473430f
iamSK77 <sk77.mobilefix@gmail.com>
2020-08-30 22:22:22 +0530

    Updates

---
+++ precache-manifest.cc862a681365e63e23127f382848a7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07258918da028dbc24f3e563688f7b42",
+    "url": "/gitname-react/index.html"
+  },
+  {
+    "revision": "a06ac60707dcba73e3c2",
+    "url": "/gitname-react/static/css/main.9de842e5.chunk.css"
+  },
+  {
+    "revision": "0b2ea87522adf3102eda",
+    "url": "/gitname-react/static/js/2.ce5ad215.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitname-react/static/js/2.ce5ad215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06ac60707dcba73e3c2",
+    "url": "/gitname-react/static/js/main.ee8c6c00.chunk.js"
+  },
+  {
+    "revision": "bf9a21d843b1558e05d6",
+    "url": "/gitname-react/static/js/runtime-main.7f9fffb1.js"
+  }
+]);
