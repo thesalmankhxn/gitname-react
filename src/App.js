4145cd4d5245bbe27523655fed2dd58ccc38fb34
iamSK77 <sk77.mobilefix@gmail.com>
2020-08-31 08:50:36 +0530

    minor bug fixes

--- src/App.js
+++ src/App.js
@@ -13,18 +13,25 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+  // async componentDidMount() {
+  //   this.setState( { loading: true } );
+    
+  //   const res = await axios.get(`https://api.github.com/users?client_id=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   this.setState( { users: res.data, loading: false } );
+  // }
+
+  searchUsers = async text => {
     this.setState( { loading: true } );
     
-    const res = await axios.get(`https://api.github.com/users?client_id=
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState( { users: res.data, loading: false } );
+    this.setState( { users: res.data.items, loading: false } );
   }
-
-  searchUsers = text => console.log(text);
 
   render() {
     return (
